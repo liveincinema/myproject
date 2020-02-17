084d1b59d4b207967d64bbe26f6c99bbd2842188
liveincinema <59525530+liveincinema@users.noreply.github.com>
2020-02-17 10:45:54 +0900

    Add files via upload

---
+++ 4주차 과제.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# select를 이용해서, tr들을 불러오기
+songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
+
+rank = 1
+for song in songs:
+    a_tag = song.select_one('a.title.ellipsis')
+
+    if a_tag is not None:
+        title = a_tag.text
+        singer = song.select_one('a.artist.ellipsis').text
+
+        doc = {
+            'rank': rank,
+            'title': title,
+            'singer': singer
+        }
+        db.top_songs.insert_one(doc)
+        rank += 1
